34fd0064a1a29d2bb2d29487eb3b5ce8ffe2e6fd
Smollny <smolllny@gmail.com>
2022-02-14 22:32:03 +0300

    update bugs

--- Task_16_7_1.Tests/Task_16_7_1_UnitTest.cs
+++ Task_16_7_1.Tests/Task_16_7_1_UnitTest.cs
@@ -3,61 +3,62 @@ using System;
 
 namespace Task_16_7_1.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class CalculatorTests
     {
         [Test]
-        public void Success_Test_10_Add_20_Return_30()
+        public void Additional_MustReturnCorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.AreEqual(30, calculator.Additional(10, 20));
         }
 
         [Test]
-        public void Fail_Test_10_Add_20_Return_10()
+        public void Additional_MustReturnUncorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.AreNotEqual(10, calculator.Additional(10, 20));
         }
         [Test]
-        public void Success_Test_20_Subtract_10_Return_10()
+        public void Subtraction_MustReturnCorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.AreEqual(10, calculator.Subtraction(20, 10));
         }
         [Test]
-        public void Fail_Test_20_Subtract_10_Return_1()
+        public void Subtraction_MustReturnUncorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.AreNotEqual(1, calculator.Subtraction(20, 10));
         }
 
         [Test]
-        public void Success_Test_20_Miltiply_10_Return_200()
+        public void Miltiplication_MustReturnCorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.AreEqual(200, calculator.Miltiplication(20, 10));
         }
         [Test]
-        public void Fail_Test_20_Miltiply_10_Return_20()
+        public void Miltiplication_MustReturnUncorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.AreNotEqual(20, calculator.Miltiplication(20, 10));
         }
 
         [Test]
-        public void Success_Test_20_Divide_10_Return_2()
+        public void Division_MustReturnCorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.AreEqual(2, calculator.Division(20, 10));
         }
         [Test]
-        public void Fail_Test_20_Divide_2_Return_2()
+        public void Division_MustReturnUncorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.AreNotEqual(2, calculator.Division(20, 2));
         }
         [Test]
-        public void Success_Test_20_Divide_0_Return_Exception()
+        public void Division_MustReturnDivideByZeroException()
         {
             Calculator calculator = new Calculator();
             Assert.Throws<DivideByZeroException>(() => calculator.Division(20, 0));
